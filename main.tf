279d2a5e3e0242025f41749ac11d006715c805c2
ramana152 <ramana152@gmail.com>
2018-12-15 14:28:42 +0530

    chenged file

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+    access_key = "${var.accesskey}"
+    secret_key = "${var.secretkey}"
+    region = "ap-southeast-1"
+  }
+  resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all"
+  }
+}
+resource "aws_instance" "aws" {
+  ami = "ami-037833522da1d1619"
+  key_name = "ramana"
+  instance_type = "t2.micro" 
+  vpc_security_group_ids = ["${aws_security_group.allow_all.id}"] 
+
+  connection {
+    user = "ubuntu"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "ansible" {
+    inline = [
+        "ansible-playbook tomcat7.yml"
+        ]
+  }
+
+}
